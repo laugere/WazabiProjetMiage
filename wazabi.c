d804f42ec8f9aeb021972af19d6977c2301cb89d
laugere <martinlaugere37@gmail.com>
2020-11-17 10:23:09 +0100

    adding type joueur and carte

--- wazabi.c
+++ wazabi.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct joueur joueur;
+struct joueur
+{
+    char pseudo[25];
+};
+
+typedef struct carte carte;
+struct carte
+{
+    int index;
+    int cout;
+    char effet[255];
+    carte *suivant;
+};
+
 int main()
 {
     srand(time(NULL));
