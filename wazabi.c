65955e3b8896bc8ca108c377121ac948df76e221
laugere <martinlaugere37@gmail.com>
2020-11-25 18:33:19 +0100

    fix

--- wazabi.c
+++ wazabi.c
@@ -79,7 +79,7 @@ int main(int argc, char *argv[])
     listeJoueurs listeJoueur;
 
     // Initialisation de la partie
-    initPartie(&listeJoueur);
+    //initPartie(&listeJoueur);
 
     //////////////////////
     //////// TEST ////////
